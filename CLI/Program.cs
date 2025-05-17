f44ffbc26976bc8b137b5db845cea636a8258f25
Jesper Højlund <github@hillgrove.dk>
2025-05-17 23:25:15 +0200

    Add ELT pipeline job and clean up project files

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,13 +1,12 @@
 ﻿using Extract;
 using Load;
 
-
 //LocationParser.Load("data/postnumre.json");
 //ProgrammingLanguageParser.Load("data/programming_languages.json");
 
-//await Jobindex.Extract();
+await Jobindex.Extract();
 
-//string searchQuery = "software engineer OR udvikler OR programmør OR fullstack OR frontend OR backend OR web OR app OR database";
-//await SerpApi.Extract(searchQuery);
+string searchQuery = "software engineer OR udvikler OR programmør OR fullstack OR frontend OR backend OR web OR app OR database";
+await SerpApi.Extract(searchQuery);
 
 await GcsUploader.UploadAllFilesAsync("data/raw", "jobdata-pipeline", "raw");
